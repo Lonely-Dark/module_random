fef6f9c59e02591a85c3f1cce7f61f81b456a86f
Lonely Dark <42666581+Lonely-Dark@users.noreply.github.com>
2019-08-12 20:19:31 +0300

    Update and rename chisl.py to main.py

--- chisl.py
+++
@@ -1,6 +0,0 @@
-import random
-while True:
- x=0
- z=1000
- score=random.randint(x, z)
- print(score)
---
+++ main.py
@@ -0,0 +1,8 @@
+#Python 3.7.3
+#Make by Lonely-Dark
+
+import random
+
+y=1000
+while True:
+ print(random.randint(0,y))
